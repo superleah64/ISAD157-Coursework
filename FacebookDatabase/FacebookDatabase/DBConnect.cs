42a57f28cc198b93a62f90aba6fe3fa812685c05
Leah Humphries <leah.humphries@students.plymouth.ac.uk>
2020-05-09 18:15:51 +0100

    Started building front-end system
    Created the database connection class and forms

---
+++ FacebookDatabase/FacebookDatabase/DBConnect.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookDatabase
+{
+    class DBConnect
+    {
+        internal const string USER_NAME = "ISAD157_LHumphries";
+
+        internal const string SERVER = "proj-mysql.uopnet.plymouth.ac.uk";
+
+        internal const string DATABASE_NAME = "ISAD157_LHumphries";
+
+        internal const string PASSWORD = "ISAD157_22222199";
+
+        internal const string SslMode = "none";
+    }
+}
